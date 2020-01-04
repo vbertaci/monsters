74181f87d67ab6b1dd1090ba8eefddf8d46692f5
Victor Bertaci <v-bertaci@hotmail.com>
2020-01-04 11:32:09 -0300

    Adicionando arquivos para paginas do git

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import './App.css';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+}
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(rsp => rsp.json())
+    .then(users => this.setState({ monsters: users }));
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  }
+
+  render() {
+    /* 
+      Esse metodo abaixo:
+        const { monsters, searchField} = this.state;
+
+      eh uma funcao que extrai, chamada de destructor, ela simplesmente seta os valores do state nas variaveis abaixo, eh o mesmo que:
+      const monster = this.state.monster;
+      const searchField = this.state.searchField;
+    */
+    const { monsters, searchField} = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+        monster.name.toLowerCase().includes(searchField.toLowerCase())
+      )
+    return (
+      <div className="App">
+      <h1> Monster Rolodex </h1>
+        <SearchBox
+          placeholder='Search monsters'
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters}  />
+      </div>
+    );
+  }
+}
+
+export default App;
