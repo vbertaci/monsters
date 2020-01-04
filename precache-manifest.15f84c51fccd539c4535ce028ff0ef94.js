0c6807e171d7433a231222891dc60948e460b4b7
Victor Bertaci <v-bertaci@hotmail.com>
2020-01-04 11:31:19 -0300

    Updates

---
+++ precache-manifest.15f84c51fccd539c4535ce028ff0ef94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b4e45caccc5e14712d22a0126508d55",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "d92902e8b33215cacd7e",
+    "url": "/monsters/static/css/main.b929db5c.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d92902e8b33215cacd7e",
+    "url": "/monsters/static/js/main.e8f89af6.chunk.js"
+  },
+  {
+    "revision": "475d84c361081acc4a17",
+    "url": "/monsters/static/js/runtime-main.05e5ea3c.js"
+  }
+]);
